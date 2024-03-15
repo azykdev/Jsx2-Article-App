91828b2856d7f0b1b96bbe548b662d8c1727824e
azykdev <azyk0106@gmail.com>
2024-03-15 11:50:17 +0500

    #9 - Localstorage & token

--- src/store/slice/auth.js
+++ src/store/slice/auth.js
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { setItem } from "../../helpers/persistance-storage";
 
 const initialState = {
   user: null,
   loading: false,
-  error: null,  
+  error: null,
   loggedIn: false,
-}
+};
 
 export const authSlice = createSlice({
   name: "auth",
@@ -19,14 +20,15 @@ export const authSlice = createSlice({
       state.loading = false;
       state.loggedIn = true;
       state.user = action.payload;
+
+      setItem("token", action.payload.token);
     },
     signFailed: (state, action) => {
       state.loading = false;
       state.error = action.payload;
-    }
-
+    },
   },
 });
 
 export const { signStart, signSuccess, signFailed } = authSlice.actions;
-export default authSlice.reducer
+export default authSlice.reducer;
